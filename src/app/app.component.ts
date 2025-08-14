67a5d14050e070346a46b9756f56ade58c7f404c
Azeem <Azeemtk9@gmail.com>
2025-08-14 15:38:22 +0530

    Routing without lazy load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
+//import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
  imports: [RouterOutlet, 
   HeaderComponent, 
   FooterComponent,
-  HomeComponent
-
+  //HomeComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {  RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule,FormsModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
