0ed2342691b8937f4cfe60943c17a56023e75be3
Azeem <Azeemtk9@gmail.com>
2025-08-14 15:05:18 +0530

    Initial commit to setup ui page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+ imports: [RouterOutlet, 
+  HeaderComponent, 
+  FooterComponent,
+  HomeComponent
+
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
