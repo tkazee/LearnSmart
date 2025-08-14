5b0694ddd872c8a4112a5ae06a95ae0d9c66dd3f
Azeem <Azeemtk9@gmail.com>
2025-08-14 15:41:34 +0530

    Refactor routing to use lazy loading for HomeComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,9 @@ import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent },
+    {path: 'home', 
+        loadChildren:()=> import('./components/home/home.component').then(m => m.HomeComponent)
+     },
 
     {
         path:'*', redirectTo: '/home'
